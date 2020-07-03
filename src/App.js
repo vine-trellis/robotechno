da73c2d9ce3c8d522c3f29aef7839b0abd55a22c
Daniel Lee <djklee@edu.uwaterloo.ca>
2020-07-02 20:43:45 -0400

    Add notepicker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import React from 'react';
 import './App.css';
 import Notepicker from './Notepicker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCog, faLink } from '@fortawesome/free-solid-svg-icons';
+
+import { Navbar, Row, Col, Button, Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <Notepicker />
+    <>
+      <Navbar>
+        <Navbar.Brand href="#">Robotechno</Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <FontAwesomeIcon icon={faLink}/>
+          </Navbar.Text>
+          <Navbar.Text>
+            <FontAwesomeIcon icon={faCog}/>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col md={12}>
+            <Notepicker />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <Button>trumpet</Button>
+            <Button>snare</Button>
+            <Button>saxophone</Button>
+          </Col>
+          <Col md={3}>
+            {/* <Button>play</Button> */}
+            <Button>change time signature</Button>
+
+          </Col>
+          <Col md={3}>
+            <Button>submit</Button>
+
+          </Col>
+        </Row>
+      </Container>
+
+
+
+    </>
   );
 }
 
