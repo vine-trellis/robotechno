f67b210df69e8a9da44a09ebd90784ae3250a7b5
Daniel Lee <djklee@edu.uwaterloo.ca>
2020-07-16 14:45:27 -0400

    Update notepicker

--- src/App.js
+++ src/App.js
@@ -10,45 +10,7 @@ import { Navbar, Row, Col, Button, Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <>
-      <Navbar>
-        <Navbar.Brand href="#">Robotechno</Navbar.Brand>
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            <FontAwesomeIcon icon={faLink}/>
-          </Navbar.Text>
-          <Navbar.Text>
-            <FontAwesomeIcon icon={faCog}/>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Navbar>
-      <Container>
-        <Row>
-          <Col md={12}>
-            <Notepicker />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <Button>trumpet</Button>
-            <Button>snare</Button>
-            <Button>saxophone</Button>
-          </Col>
-          <Col md={3}>
-            {/* <Button>play</Button> */}
-            <Button>change time signature</Button>
-
-          </Col>
-          <Col md={3}>
-            <Button>submit</Button>
-
-          </Col>
-        </Row>
-      </Container>
-
-
-
-    </>
+    <Notepicker />
   );
 }
 
--- src/Notepicker.js
+++ src/Notepicker.js
@@ -1,62 +1,153 @@
 import React, { useState } from 'react';
-import { Table, Container, Button } from 'react-bootstrap';
+import { Table, Container, Button, Row, Col, Navbar, InputGroup, Form, Image } from 'react-bootstrap';
 import { Instrument } from 'musical.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faVolumeUp, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
+import drums from './assets/instruments/drums.png';
+import triangle from './assets/instruments/triangle.png';
+import trumpet from './assets/instruments/trumpet.png';
 import './Notepicker.css';
 
-const SEMITONES = ['A', '^A', 'B', 'C', '^C', 'D', '^D', 'E', 'F', '^F', 'G', '^G'];
-const NUM_BEATS = 16;
-const INITIAL_NOTES = new Array(NUM_BEATS).fill('z');
+const SEMITONES = ['C', '^C', 'D', '^D', 'E', 'F', '^F', 'G', '^G', 'A', '^A', 'B'].reverse();
 
 export default function Notepicker() {
   let inst = new Instrument();
+  const [username, setUsername] = useState('');
+  const [bpm, setBpm] = useState(200);
+  const [numberNotes, setNumberNotes] = useState(4);
+  const [numberSubdivisions, setNumberSubdivisions] = useState(4);
+  const INITIAL_NOTES = new Array(numberNotes * numberSubdivisions).fill('z');
   const [notes, setNotes] = useState(INITIAL_NOTES);
-  const [bpm, setBpm] = useState(120);
+  const [instrument, setInstrument] = useState('trumpet');
 
   return (
-    <>
-      <Table bordered>
-        <tbody>
-          {
-            SEMITONES.map((tone) => {
-              return <tr key={tone}>
-                {[...Array(NUM_BEATS).keys()].map((i) => {
-                  return <td 
-                    className={notes[i] === tone ? "selected-note" : "unselected" }
-                    key={i + tone}
-                    onClick={() => {
-                      inst.tone(tone);
-                      let temp_notes = notes.slice();
-                      temp_notes[i] = tone;
-                      setNotes(temp_notes);
-                    }}>
+    <Container>
+      <Navbar>
+        <Navbar.Brand href="#">Robotechno</Navbar.Brand>
+        <Navbar.Brand>
+          <InputGroup>
+            <Form.Control as="input" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+            <InputGroup.Append>
+              <Button disabled variant="outline-secondary">
+                <FontAwesomeIcon icon={faPencilAlt} />
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Help
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+      <Row className="my-3">
+        <Col md={{ span: 3, offset: 4 }}>
+          <Form.Group as={Row}>
+            <Form.Label column>
+              # Notes:
+          </Form.Label>
+            <Col md={4}>
+              <Form.Control as="select" value={numberNotes} onChange={(e) => setNumberNotes(e.target.value)}>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={6}>6</option>
+              </Form.Control>
+            </Col>
+          </Form.Group>
+        </Col>
+        <Col md={{ span: 3 }}>
+          <Form.Group as={Row}>
+            <Form.Label column>
+              # Note subdivisions:
+          </Form.Label>
+            <Col md={4}>
+              <Form.Control as="select" value={numberSubdivisions} onChange={(e) => setNumberSubdivisions(e.target.value)}>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+              </Form.Control>
+            </Col>
+          </Form.Group>
+        </Col>
+        <Col md={{ span: 2 }} className='right-button'>
+          <Button
+            variant="outline-dark"
+            onClick={() => {
+              setNumberNotes(4);
+              setNumberSubdivisions(4);
+              setNotes(INITIAL_NOTES);
+            }}>Clear All</Button>
+        </Col>
+      </Row>
+      <Row className="my-2">
+        <Col>
+          <Table bordered>
+            <tbody>
+              {
+                SEMITONES.map((tone) => {
+                  return <tr key={tone}>
+                    <td className='note-label'>
                       {tone}
                     </td>
-                })}
-              </tr>
-            })
-          }
-        </tbody>
-      </Table>
-      <Button onClick={() => {
-        console.log(notes.join(""));
-        inst.play({tempo: bpm},notes.join(""),);
-      }}>
-        Play
-      </Button>
-    </>
+                    {[...Array(numberNotes * numberSubdivisions).keys()].map((i) => {
+                      return <td
+                        className={notes[i] === tone ? "selected-note" : "unselected-note"}
+                        key={i + tone}
+                        onClick={() => {
+                          inst.tone(tone);
+                          let temp_notes = notes.slice();
+                          temp_notes[i] = tone;
+                          setNotes(temp_notes);
+                        }} />
+                    })}
+                  </tr>
+                })
+              }
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+      <Row className="my-2">
+        <Col md={{ span: 3, offset: 1 }}>
+          <Button onClick={() => {
+            console.log(notes.join(""));
+            inst.play({ tempo: bpm }, notes.join(""));
+          }}
+            className="mx-1"
+          ><FontAwesomeIcon icon={faPlay} /></Button>
+          <Button
+            className="mx-1"
+          ><FontAwesomeIcon icon={faVolumeUp} /></Button>
+        </Col>
+        <Col>
+          <div id='instrument-picker'>
+            <Image
+              className={instrument === 'drums' ? 'selected-instrument' : 'unselected-instrument'}
+              onClick={() => setInstrument('drums')}
+              src={drums}
+            />
+            <Image
+              className={instrument === 'triangle' ? 'selected-instrument' : 'unselected-instrument'}
+              onClick={() => setInstrument('triangle')}
+              src={triangle}
+            />
+            <Image
+              className={instrument === 'trumpet' ? 'selected-instrument' : 'unselected-instrument'}
+              onClick={() => setInstrument('trumpet')}
+              src={trumpet}
+            />
+          </div>
+        </Col>
+
+      </Row>
+      <Row className="my-2">
+        <Col md={{ span: 2, offset: 10 }} className="right-button">
+          <Button variant="dark">
+            Submit
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
-
-// function Note({ inst, tone, setNote, selected }) {
-//   return (
-//     <td 
-//     className={selected ? "selected-note" : ""}
-//     onClick={() => {
-//       inst.tone(tone);
-//       setNote();
-//     }}>
-//       {tone}
-//     </td>
-//   )
-// }
